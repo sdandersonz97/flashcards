422f5130d2fd7857d948e4b4f3a022aad9c253a1
Steven Anderson <sdandersonz97@gmail.com>
2017-10-11 11:26:00 -0400

    feat: auth reducer

---
+++ src/reducers/auth.js
@@ -0,0 +1,32 @@
+import { LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from '../actions/types'
+
+const INITIAL_STATE = { 
+    loading: false,
+    error: '',
+    user: null
+}
+export default (state=INITIAL_STATE, action) => {
+    switch(action.type){
+        case LOGIN_USER:
+            return { 
+                ...state, 
+                loading: true, 
+                error: '' 
+            }
+        case LOGIN_USER_SUCCESS:
+            return { 
+                ...state,
+                ...INITIAL_STATE,
+                user: action.payload, 
+            }
+        case LOGIN_USER_FAIL:
+            return { 
+                ...state, 
+                error: "Authenticated Failed", 
+                password: '', 
+                loading: false 
+            }
+        default:
+            return state
+    }
+}
