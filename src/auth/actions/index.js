231c06e1f9c17825004b13cb3c1f799e504e50c3
Steven Anderson <sdandersonz97@gmail.com>
2017-10-11 11:16:01 -0400

    feat: auth actions

---
+++ src/auth/actions/index.js
@@ -0,0 +1,31 @@
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from './types'
+import firebase from 'firebase'
+
+export const loginUser = ({ email, password },cb) => {
+    return dispatch => {
+     dispatch({ type: LOGIN_USER })
+     firebase.auth().signInWithEmailAndPassword(email, password)
+         .then(user  => {
+             loginUserSuccess(dispatch, user)
+             cb()
+         })
+         .catch(() => {
+             firebase.auth().createUserWithEmailAndPassword(email, password)
+                 .then(user => loginUserSuccess(dispatch, user))
+                 .catch(() => loginUserFail(dispatch))
+         })
+     }
+ }
+ 
+ const loginUserFail = dispatch => {
+     dispatch({
+         type: LOGIN_USER_FAIL
+     })
+ }
+ 
+ const loginUserSuccess = ( dispatch, user ) => {
+     dispatch({
+         type: LOGIN_USER_SUCCESS, 
+         payload: user
+     })
+ }
