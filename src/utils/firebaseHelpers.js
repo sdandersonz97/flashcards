a74c28cb95c0c4c8d4b7a38faea0751eba9a9b22
Steven Anderson <sdandersonz97@gmail.com>
2017-10-12 14:12:43 -0400

    feat: new firebase helper

--- src/utils/firebaseHelpers.js
+++ src/utils/firebaseHelpers.js
@@ -3,7 +3,7 @@ import firebase from 'firebase'
 export const rootRef = () => firebase.database().ref()
 export const userRef = uid => rootRef().child(`users/${uid}`)
 export const userDecksRef = uid => userRef(uid).child('decks')
-
+export const userDeckQuestionsRef = (uid, deckId) => userDecksRef(uid).child(`${deckId}/questions`)
 export const getCurrentUser = () => {
     return firebase.auth().currentUser
 }
