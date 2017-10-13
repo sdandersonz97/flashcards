8bbff8a3e2e9755f78ab3bce218e188f7e8981ff
Steven Anderson <sdandersonz97@gmail.com>
2017-10-13 15:53:40 -0400

    feat: fetch public decks action

--- src/public/actions/index.js
+++ src/public/actions/index.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase'
 import { userDecksRef, getCurrentUser, userDeckQuestionsRef, publicDecksRef } from '../../utils/firebaseHelpers'
-import { FETCH_USER_DECKS, ADD_USER_DECK, ADD_USER_CARD_TO_DECK, ADD_PUBLIC_DECK } from './types'
+import { FETCH_USER_DECKS, ADD_USER_DECK, ADD_USER_CARD_TO_DECK, ADD_PUBLIC_DECK, FETCH_PUBLIC_DECKS } from './types'
 
 
 export const fetchUserDecks = () => {
@@ -46,4 +46,14 @@ const addPublicDeck = (deckId, uid, category) => {
         uid,
         likes:0
     })
+}
+
+export const fetchPublicDecks = () => {
+    return () => publicDecksRef().limitToFirst(20).on('value', snap => {
+        dispatch({
+            type: FETCH_PUBLIC_DECKS,
+            decks: snap.val()
+        })
+    })
+
 }
