50a38be268711e9d8e421cbdb88191eb89b4bab4
Steven Anderson <sdandersonz97@gmail.com>
2017-10-13 15:40:25 -0400

    feat: public deck list

---
+++ src/public/containers/publicDeckList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { View, FlatList, Text } from 'react-native'
+import { AppLoading } from 'expo'
+import { fontStyles, containersStyles } from '../../styles' 
+import { Deck, Spinner } from '../../common'
+import { fetchPublicDecks } from '../actions'
+
+class publicDecksList extends Component {
+    componentDidMount(){
+        this.props.fetchPublicDecks()
+    }
+    renderDeck = ({ item }) => {
+        const { key } = item
+        const { navigate } = this.props.navigation
+        return <Deck onClickNavigate={(screen) => navigate(screen,{ key })} { ...item } />
+    }
+    render(){
+        const { decks } = this.props
+        const { titleStyle, subtitleStyle } = fontStyles
+        return !decks 
+        ? <Spinner/>
+        : <FlatList
+            data={decks}
+            renderItem={this.renderDeck}
+        />
+    }
+}
+
+const mapStateToProps = ({ publicDecks }) => {
+    return { decks: Object.keys(publicDecks).map(deck => publicDecks[deck]) }
+}
+export default connect(mapStateToProps, { fetchPublicDecks })(PublicDecksList)
