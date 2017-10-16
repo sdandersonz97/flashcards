538fd5dd7c414925568cc06886dff7cdc35d53e3
Steven Anderson <sdandersonz97@gmail.com>
2017-10-15 22:14:19 -0400

    feat: fix: bad imports

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
 import privateDecks from './privateDecks'
 import publicDecks from './publicDecks'
-import localDecks from './localDecks'
 import auth from './auth'
 export default combineReducers({
     privateDecks,
-    localDecks,
     publicDecks,
     auth
 })
