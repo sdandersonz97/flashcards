bef56512aaf00e40dce85e3aefd3c075c83bd70c
Steven Anderson <sdandersonz97@gmail.com>
2017-10-24 00:18:33 -0400

    Taking off some categories

--- src/public/components/categoryPicker.js
+++ src/public/components/categoryPicker.js
@@ -19,15 +19,8 @@ export default class CategoryPicker extends Component {
                 onValueChange={this.props.onValueChange.bind(this)}
         >
             <Item label="Select a Category" value="selectCategory" />
-            <Item label="Self-Driving Cars" value="SelfDrivingCards" />
-            <Item label="Deep Learning" value="DeepLearning" />
-            <Item label="Machine Learning" value="MachineLearning" />
-            <Item label="Artificial Intelligence" value="AI" />
-            <Item label="Data Analyst" value="DataAnalyst" />
             <Item label="Full Stack Web Developer" value="FullStack" />
             <Item label="Front End Web Developer" value="FrontEnd" />
-            <Item label="Robotics" value="Robotics" />
-            <Item label="VR" value="VR" />
             <Item label="Android" value="Android" />
             <Item label="Ios" value="Ios" />
         </Picker>
