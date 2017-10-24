e97ce960a96dfbd4ecbc99ecd2dc0bcb2730f54c
Steven Anderson <sdandersonz97@gmail.com>
2017-10-24 00:12:08 -0400

    deleting unecessary files

--- src/public/containers/categoryList.js
+++ src/public/containers/categoryList.js
@@ -11,14 +11,14 @@ class CategoryList extends Component {
             <ScrollView>
                 <View style={{flex:1, alignItems:'center', backgroundColor:'#fff'}}>
                         <CardImage 
-                            source={require('../img/robotics.jpg')} 
-                            onPressNavigate={this.onPressNavigate('Robotic', navigate)}>
-                            Robotics
-                        </CardImage>
-                        <CardImage 
                             source={require('../img/web.jpg')} 
                             onPressNavigate={this.onPressNavigate('FrontEnd', navigate)}>
                             Front End
+                        </CardImage>
+                        <CardImage 
+                            source={require('../img/fullstack.jpg')} 
+                            onPressNavigate={this.onPressNavigate('FullStack', navigate)}>
+                            Full Stack
                         </CardImage>
                         <CardImage 
                             source={require('../img/android.jpg')} 
@@ -26,20 +26,9 @@ class CategoryList extends Component {
                             Android
                         </CardImage>
                         <CardImage 
-                            source={require('../img/fullstack.jpg')} 
-                            onPressNavigate={this.onPressNavigate('FullStack', navigate)}>
-                            Full Stack
-                        </CardImage>
-
-                        <CardImage 
-                            source={require('../img/vr.jpg')} 
-                            onPressNavigate={this.onPressNavigate('VR', navigate)}>
-                            VR
-                        </CardImage>
-                        <CardImage 
-                            source={require('../img/ai.jpg')} 
-                            onPressNavigate={this.onPressNavigate('AI', navigate)}>
-                            Artificial Intelligence
+                            source={require('../img/ios.jpg')} 
+                            onPressNavigate={this.onPressNavigate('Ios', navigate)}>
+                            IOS
                         </CardImage>
                 </View>
             </ScrollView>
